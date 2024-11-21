09ec3a9e16475c712b53fe3f1dce4761075db0f8
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2024-11-21 23:58:37 +0700

    Handle only the POST request

---
+++ qr-code-generator/src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request, env, ctx) {
+		if (request.method == "POST") {
+			return new Response('Hello Worker!');
+		}
+		return new Response("Expected POST request", {
+			status: 405,
+		});
+	},
+};
